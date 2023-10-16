d24bfe80400bb7cdddcf0c64f10e46809d1c67a6
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-10-16 07:34:01 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const http = require("http");
+const { connectDB } = require("./db/dbconnection");
+const routes = require("./routes/v1");
+require("./helpers/crons");
+const config = require("./config/config");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(cors());
+app.options("*", cors());
+
+app.use(express.static(`./public`));
+
+app.use("/v1", routes);
+
+app.use((req, res, next) => {
+    next(new Error("Route not found!"));
+});
+
+connectDB();
+
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
+    console.log("server listning port number " + config.port);
+});
